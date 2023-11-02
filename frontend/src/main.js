105467c6056ead69ff021a5691ecca95c7886cde
jkennethdomingo <130147461+jkennethdomingo@users.noreply.github.com>
2023-11-02 16:18:35 +0800

    inclusion of axios and simple login
    For testing of jwtoken and connection of 2 frameworks

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://backend.test/api/"
 
 createApp(App).use(router).mount('#app')
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,69 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="login">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          v-model="form.email"
+          required
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          v-model="form.password"
+          required
+        />
+      </div>
+
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('/login', {
+          email: this.form.email,
+          password: this.form.password,
+        });
+
+        if (response.data.token) {
+          // Store the token securely (e.g., in local storage)
+          localStorage.setItem('jwtToken', response.data.token);
+
+          // Redirect or perform other actions upon successful login
+          this.$router.push({ name: 'home' });
+        } else {
+          // Handle login failure, show an error message, etc.
+          // Example: this.$q.notify({ message: 'Login failed', color: 'negative' });
+        }
+      } catch (error) {
+        // Handle API request error, e.g., show a network error message
+        // Example: this.$q.notify({ message: 'Network error', color: 'negative' });
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
