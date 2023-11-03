a094b9acd6db9a0acb9b13a3c569ca282313827a
Jaydee <108048499+JoseDexterAnyayahan@users.noreply.github.com>
2023-11-03 14:36:12 +0800

    PageFooter

---
+++ frontend/src/components/PageFooter.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+    <footer
+        class="flex flex-col flex-shrink-0 w-full gap-2 px-6 py-4 lg:flex-row lg:justify-between"
+    >
+        <p class="text-sm text-white-100 dark:text-white-400">
+            &#169;
+            <span>{{ new Date().getFullYear() }}</span> PDEA, All rights reserved
+        </p>
+    </footer>
+</template>
