54cb2614c58a928e78082c76e2bb0e2866d6afa1
jkennethdomingo <130147461+jkennethdomingo@users.noreply.github.com>
2023-11-03 13:29:01 +0800

    inclusion of tailwind css and toast

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,19 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import axios from 'axios'
+import Toast from 'vue-toastification'
+import '@/assets/css/main.css'
 
 axios.defaults.baseURL = "http://backend.test/api/"
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(Toast, {
+  hideProgressBar: true,
+  closeOnClick: false,
+  closeButton: false,
+  icon: false,
+  timeout: false,
+  transition: 'Vue-Toastification__fade',
+})
+app.mount('#app')
