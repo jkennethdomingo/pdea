7221aea3ba0d0223180670ff77637a35ded1a9ba
ayenmilante18 <144781024+ayenmilante18@users.noreply.github.com>
2023-11-10 20:27:59 +0800

    icons

---
+++ frontend/src/components/icons/DashboardIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <!-- SVG paths for DashboardIcon -->
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'DashboardIcon'
+}
+</script>
