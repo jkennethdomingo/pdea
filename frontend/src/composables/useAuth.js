639fcbdab119703d01f17b50e47776934df7b95d
jkennethdomingo <130147461+jkennethdomingo@users.noreply.github.com>
2023-11-12 19:28:54 +0800

    minor fixes

--- frontend/src/composables/useAuth.js
+++ frontend/src/composables/useAuth.js
@@ -1,9 +1,10 @@
 import { jwtDecode } from 'jwt-decode';
-import axios from 'axios';
+import apiService from '@/composables/axios-setup'; // Import your apiService
 
 export function useAuth(router, store) {
     const performLogin = async (email, password, remember) => {
-        const response = await axios.post('/auth/login', { email, password });
+        // Use apiService instance instead of axios
+        const response = await apiService.post('/auth/login', { email, password });
         const decodedToken = jwtDecode(response.data.token);
 
         store.commit('setAuth', { token: response.data.token, role: decodedToken.role });
