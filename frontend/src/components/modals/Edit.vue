4aa127656db539cfb7f78f57484146254f630d91
ayenmilante18 <144781024+ayenmilante18@users.noreply.github.com>
2023-11-16 22:30:05 +0800

    add edit v-model

--- frontend/src/components/modals/Edit.vue
+++ frontend/src/components/modals/Edit.vue
@@ -11,72 +11,80 @@
                 </button>
                 <br>
                 <div class="mb-4 grid grid-cols-3 gap-4">
-                    <div class="md:px-8">
+                    <div>
+                        <label for="Date" class="block text-gray-700 text-sm dark:text-white mb-2">Procurement Status:</label>
+                        <input type="date" id="Date" v-model="formData.status_name" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white  leading-tight  focus:outline-none focus:shadow-outline">
+                    </div>
+                    <div>
                         <label for="Date" class="block text-gray-700 text-sm dark:text-white mb-2">Date of Receipt of Request:</label>
-                        <input type="date" id="Date" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white  leading-tight  focus:outline-none focus:shadow-outline">
+                        <input type="date" id="Date" v-model="formData.date_of_receipt_of_request" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white  leading-tight  focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Project" class="block text-gray-700 text-sm dark:text-white mb-2">Project/Particulara:</label>
-                        <input type="text" id="Project"  class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Project" v-model="formData.project_particular"  class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="End" class="block text-gray-700 text-sm dark:text-white mb-2">End-User:</label>
-                        <input type="text" id="End" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <select type="dropdown" id="End" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                        <option value="option3">Option 3</option>
+                        </select>
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Purchase" class="block text-gray-700 text-sm dark:text-white mb-2">Purchase/Work/Job Request:</label>
-                        <input type="text" id="Purchase" placeholder="No" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-                        <input type="text" id="Purchase" placeholder="Date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Purchase1" placeholder="No" v-model="formData.purchase_work_job_request_no" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="date" id="Purchase2" placeholder="Date" v-model="formData.purchase_work_job_request_date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Philgeps" class="block text-gray-700 text-sm dark:text-white mb-2">Philgeps:</label>
-                        <input type="text" id="Philgeps" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="checkbox" id="Philgeps" v-model="formData.philgeps_posting" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Price" class="block text-gray-700 text-sm dark:text-white mb-2">Price Quotation:</label>
-                        <input type="text" id="Price" placeholder="No" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-                        <input type="text" id="Price" placeholder="Date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Price1" placeholder="No" v-model="formData.price_quotation_no" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="date" id="Price2" placeholder="Date" v-model="formData.price_quotation_date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Abstract" class="block text-gray-700 text-sm dark:text-white mb-2">Abstract of Canvass:</label>
-                        <input type="text" id="Abstract" placeholder="No" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-                        <input type="text" id="Abstract" placeholder="Date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Abstract1" placeholder="No" v-model="formData.abstract_of_canvas_no" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="date" id="Abstract2" placeholder="Date" v-model="formData.abstract_of_canvas_date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Amount" class="block text-gray-700 text-sm dark:text-white mb-2">Amount:</label>
-                        <input type="text" id="Amount" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Amount" v-model="formData.amount" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Supplier" class="block text-gray-700 text-sm dark:text-white mb-2">Supplier:</label>
-                        <input type="text" id="Supplier" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Supplier" v-model="formData.supplier" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Date_Request" class="block text-gray-700 text-sm dark:text-white mb-2">Date Request for Fund:</label>
-                        <input type="text" id="Date_Request" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="date" id="Date_Request" v-model="formData.date_request_for_fund" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Ideal_No" class="block text-gray-700 text-sm dark:text-white mb-2">Ideal No. of Days to Complete:</label>
-                        <input type="text" id="Ideal_No" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Ideal_No" v-model="formData.idel_no_of_days_to_complete" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Actual_days" class="block text-gray-700 text-sm dark:text-white mb-2">Actual Days Completed:</label>
-                        <input type="text" id="Actual_days" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Actual_days" v-model="formData.actual_days_to_complete" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Difference" class="block text-gray-700 text-sm dark:text-white mb-2">Difference:</label>
-                        <input type="text" id="Difference" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Difference" v-model="formData.difference" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
-                        <label for="Purchase" class="block text-gray-700 text-sm dark:text-white mb-2">Purchase/Work/Job Order:</label>
-                        <input type="text" id="Purchase" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                    <div>
+                        <label for="PurchaseOrder" class="block text-gray-700 text-sm dark:text-white mb-2">Purchase/Work/Job Order:</label>
+                        <input type="text" id="PurchaseOrder" v-model="formData.purchase_order" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Delivery_Status" class="block text-gray-700 text-sm dark:text-white mb-2">Delivery Status:</label>
-                        <input type="text" id="Delivery_Status" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Delivery_Status" v-model="formData.delivery_status" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
-                    <div class="md:px-8">
+                    <div>
                         <label for="Remarks" class="block text-gray-700 text-sm dark:text-white mb-2">Remarks:</label>
-                        <input type="text" id="Remarks" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+                        <input type="text" id="Remarks" v-model="formData.remarks" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
                     </div>
                 </div>
                 <div class="p-4 md:px-8">
@@ -91,6 +99,30 @@
 
 <script setup>
 import { defineProps, defineEmits } from 'vue';
+const formData = ref({
+  status_name: '',
+  date_of_receipt_of_request: '',
+  project_particular: '',
+  endUser: '',
+  purchase_work_job_request_no: '',
+  purchase_work_job_request_date: '',
+  philgeps_posting: '',
+  price_quotation_no: '',
+  price_quotation_date: '',
+  abstract_of_canvas_no: '',
+  abstract_of_canvas_date: '',
+  amount: '',
+  supplier: '',
+  date_request_for_fund: '',
+  idel_no_of_days_to_complete: '',
+  actual_days_to_complete: '',
+  difference: '',
+  purchase_order: '',
+  delivery_status: '',
+  remarks: '',
+  // ... add all other form fields here
+});
+
 
 const props = defineProps({
   isVisible: Boolean,
