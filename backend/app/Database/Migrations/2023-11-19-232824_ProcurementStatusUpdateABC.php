0d6ee6e2c8d8a6f8f115ae0969f32bddd7c4c2ca
ayenmilante18 <144781024+ayenmilante18@users.noreply.github.com>
2023-11-20 20:32:02 +0800

    procurement update

--- backend/app/Database/Migrations/2023-11-19-232824_ProcurementStatusUpdateABC.php
+++ backend/app/Database/Migrations/2023-11-19-232824_ProcurementStatusUpdateABC.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class ProcurementStatusUpdate extends Migration
+class ProcurementStatusUpdateABC extends Migration
 {
     public function up()
     {
