d4dabb7f657a24cd090bf9fa39d9cf3de5e6a929
jkennethdomingo <130147461+jkennethdomingo@users.noreply.github.com>
2023-11-22 22:13:31 +0800

    async add

--- frontend/src/views/hr/assign_training/AssignTraining.vue
+++ frontend/src/views/hr/assign_training/AssignTraining.vue
@@ -82,7 +82,6 @@ function openAddEventDialog() {
 }
 
 function handleDateSelect(selectInfo) {
-  // Handle date selection
 }
 
 function handleEventClick(clickInfo) {
@@ -97,15 +96,98 @@ function handleWeekendsToggle() {
   // Handle weekends toggle
 }
 
-function onModalClose() {
-  // Logic to execute when the modal is closed
-  console.log('Modal closed');
-}
+const newEvent = ref({
+  title: '',
+  period_from: '',
+  period_to: '',
+  number_of_hours: '',
+  conducted_by: ''
+});
+
+const addEvent = async () => {
+  await store.dispatch('addEvent', newEvent.value);
+  await store.dispatch('getTraining');
+  // Reset newEvent here
+  newEvent.value = { title: '', period_from: '', period_to: '', number_of_hours: '', conducted_by: '' };
+};
+
 </script>
 
 
 <template>
-<AddTrainingModal @close="onModalClose"/>
+
+    <!-- drawer init and toggle -->
+    <div class="text-center">
+        <button 
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" 
+            type="button" 
+            data-drawer-target="dynamic-drawer" 
+            data-drawer-show="dynamic-drawer" 
+            data-drawer-placement="right" 
+            aria-controls="dynamic-drawer">
+            Show right drawer
+        </button>
+    </div>
+
+    <!-- dynamic drawer component -->
+    <div 
+        id="dynamic-drawer" 
+        class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-80 dark:bg-gray-800" 
+        tabindex="-1" 
+        aria-labelledby="drawer-label">
+        
+        <h5 id="drawer-label" class="inline-flex items-center mb-6 text-base font-semibold text-gray-500 uppercase dark:text-gray-400"><svg class="w-3.5 h-3.5 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm14-7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm-5-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm-5-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4Z"/>
+        </svg>New event</h5>
+        <button type="button" data-drawer-hide="dynamic-drawer" aria-controls="dynamic-drawer" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white" >
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Close menu</span>
+        </button>
+
+        <form @submit.prevent="addEvent" class="mb-6">
+        <!-- Title Field -->
+        <div class="mb-6">
+            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+            <input type="text" v-model="newEvent.title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Event Title" required>
+        </div>
+
+        <!-- Period From Field -->
+        <div class="mb-6">
+            <label for="period_from" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Period From</label>
+            <input type="date" v-model="newEvent.period_from" id="period_from" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        </div>
+
+        <!-- Period To Field -->
+        <div class="mb-6">
+            <label for="period_to" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Period To</label>
+            <input type="date" v-model="newEvent.period_to" id="period_to" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        </div>
+
+        <!-- Number of Hours Field -->
+        <div class="mb-6">
+            <label for="number_of_hours" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of Hours</label>
+            <input type="number" v-model="newEvent.number_of_hours" id="number_of_hours" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        </div>
+
+        <!-- Conducted By Field -->
+        <div class="mb-6">
+            <label for="conducted_by" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Conducted By</label>
+            <input type="text" v-model="newEvent.conducted_by" id="conducted_by" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Conductor's Name" required>
+        </div>
+
+        <!-- Submit Button -->
+        <button type="submit" class="text-white justify-center flex items-center bg-blue-700 hover:bg-blue-800 w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            <svg class="w-3.5 h-3.5 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M18 2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2ZM2 18V7h6.7l.4-.409A4.309 4.309 0 0 1 15.753 7H18v11H2Z"/>
+                <path d="M8.139 10.411 5.289 13.3A1 1 0 0 0 5 14v2a1 1 0 0 0 1 1h2a1 1 0 0 0 .7-.288l2.886-2.851-3.447-3.45ZM14 8a2.463 2.463 0 0 0-3.484 0l-.971.983 3.468 3.468.987-.971A2.463 2.463 0 0 0 14 8Z"/>
+            </svg> Create event
+        </button>
+    </form>
+        <!-- Dynamic content goes here -->
+        
+    </div>
     <div class='flex min-h-full font-sans text-sm'>
         <div class="text-center section">
           <div class="hidden lg:flex">
