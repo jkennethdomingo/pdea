f6d91eed4a51ea652366fdddca6d43e705344d81
ayenmilante18 <144781024+ayenmilante18@users.noreply.github.com>
2023-11-23 23:52:39 +0800

    property

--- frontend/src/views/logistics/manage_inventory/LG_Property_Add.vue
+++ frontend/src/views/logistics/manage_inventory/LG_Property_Add.vue
@@ -11,28 +11,19 @@ const procurementStatus = procurementStatusData.statuses;
 const store = useStore();
 // Create a reactive object for your form data
 const formData = ref({
-  status_name: '',
-  date_of_receipt_of_request: '',
-  project_particulars: '',
-  endUser: '',
-  purchase_work_job_request_no: '',
-  purchase_work_job_request_date: '',
-  philgeps_posting: '',
-  price_quotation_no: '',
-  price_quotation_date: '',
-  abstract_of_canvas_no: '',
-  abstract_of_canvas_date: '',
-  amount: '',
-  supplier: '',
-  date_request_for_fund: '',
-  ideal_no_of_days_to_complete: '',
-  actual_days_to_complete: '',
-  difference: '',
-  purchase_order: '',
-  delivery_status: '',
-  remarks: '',
-  endUserType: '',
-  // ... add all other form fields here
+  asset_type_name: '',
+  description: '',
+  yr_acquired: '',
+  serial_number: '',
+  property_number: '',
+  unit_of_measure: '',
+  unit_value: '',
+  qty_per_property_card: '',
+  physical_count: '',
+  shortage_overage_qty: '',
+  shortage_overage_value: '',
+  status: '',
+  remarks_whereabouts: '',
 });
 
 onMounted(() => {
@@ -153,96 +144,65 @@ const submitForm = async () => {
 
 <template>
   <PageWrapper>
-  
-  <br>
+
   <form @submit.prevent="submitForm">
     <Button class="dark:bg-green-600" :to="{ name: 'LG_Property_Monitoring' }">View Details</Button>
-<div class="grid grid-cols-4 gap-4 pt-2">
+<div class="grid grid-cols-3 gap-4 pt-2">
       <div>
-        <label for="status_name" class="block text-gray-700 text-sm dark:text-white mb-2">Procurement Status:</label>
-        <select id="status_name" v-model="formData.status_name" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white  leading-tight  focus:outline-none focus:shadow-outline">
-          <option v-for="pc in procurementStatus" :key="pc.value" :value="pc.value">
+        <label for="asset_type_name" class="block text-gray-700 text-sm dark:text-white mb-2">Article:</label>
+        <select id="asset_type_name" v-model="formData.asset_type_name" class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white  leading-tight  focus:outline-none focus:shadow-outline">
+          <option v-for="pc in propertyStatus" :key="pc.value" :value="pc.value">
             {{ pc.label }}
           </option>
         </select>
       </div>
       <div>
-        <label for="Date" class="block text-gray-700 text-sm dark:text-white mb-2">Date of Receipt of Request:</label>
-        <input type="date" id="Date" v-model="formData.date_of_receipt_of_request" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white  leading-tight  focus:outline-none focus:shadow-outline">
+        <label for="description" class="block text-gray-700 text-sm dark:text-white mb-2">Description:</label>
+        <input type="text" id="description" v-model="formData.description" class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white  leading-tight  focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="Project" class="block text-gray-700 text-sm dark:text-white mb-2">Project/Particulars:</label>
-        <input type="text" id="Project" v-model="formData.project_particulars"  class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+        <label for="yr_acquired" class="block text-gray-700 text-sm dark:text-white mb-2">Year Acquired:</label>
+        <input type="date" id="yr_acquired" v-model="formData.yr_acquired"  class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="EndUserType" class="block text-gray-700 text-sm dark:text-white mb-2">End-User Type:</label>
-        <select id="EndUserType" v-model="formData.endUserType" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-          <option value="" disabled>Select Type</option>
-          <option v-for="option in endUserTypeOptions" :key="option.value" :value="option.value">{{ option.label }}</option>
-        </select>
+        <label for="serial_number" class="block text-gray-700 text-sm dark:text-white mb-2">Serial Number:</label>
+        <input type="text" id="serial_number" v-model="formData.serial_number"  class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="EndUser" class="block text-gray-700 text-sm dark:text-white mb-2">End-User:</label>
-        <select id="EndUser" v-model="formData.endUser" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-          <option value="" disabled>Select User</option>
-          <option v-for="option in endUserOptions" :key="option.value" :value="option.value">{{ option.label }}</option>
-        </select>
+        <label for="property_number" class="block text-gray-700 text-sm dark:text-white mb-2">Property Number:</label>
+        <input type="text" id="property_number" v-model="formData.property_number"  class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="Purchase" class="block text-gray-700 text-sm dark:text-white mb-2">Purchase/Work/Job Request:</label>
-        <input type="text" id="Purchase1" placeholder="No" v-model="formData.purchase_work_job_request_no" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-        <input type="date" id="Purchase2" placeholder="Date" v-model="formData.purchase_work_job_request_date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-      </div>
-      <div class="flex items-center mb-4">
-          <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Philgeps:</label>
-          <input id="Philgeps" type="checkbox" v-model="formData.philgeps_posting" value="1" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="unit_of_measure" class="block text-gray-700 text-sm dark:text-white mb-2">Unit of Measure:</label>
+        <input type="text" id="unit_of_measure" v-model="formData.unit_of_measure"  class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="Price" class="block text-gray-700 text-sm dark:text-white mb-2">Price Quotation:</label>
-        <input type="text" id="Price1" placeholder="No" v-model="formData.price_quotation_no" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-        <input type="date" id="Price2" placeholder="Date" v-model="formData.price_quotation_date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+        <label for="unit_value" class="block text-gray-700 text-sm dark:text-white mb-2">Unit of Value:</label>
+        <input type="text" id="unit_value" v-model="formData.unit_value"  class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="Abstract" class="block text-gray-700 text-sm dark:text-white mb-2">Abstract of Canvass:</label>
-        <input type="text" id="Abstract1" placeholder="No" v-model="formData.abstract_of_canvas_no" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-        <input type="date" id="Abstract2" placeholder="Date" v-model="formData.abstract_of_canvas_date" class="shadow border  dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+        <label for="qty_per_property_card" class="block text-gray-700 text-sm dark:text-white mb-2">Quantity Per Property Card:</label>
+        <input type="text" id="qty_per_property_card" v-model="formData.qty_per_property_card"  class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="Amount" class="block text-gray-700 text-sm dark:text-white mb-2">Amount:</label>
-        <input type="text" id="Amount" v-model="formData.amount" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+        <label for="physical_count" class="block text-gray-700 text-sm dark:text-white mb-2">Quantity Per Physical Count:</label>
+        <input type="text" id="physical_count" v-model="formData.physical_count"  class="shadow border dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+      </div> 
+      <div>
+        <label for="shortage_overage_qty" class="block text-gray-700 text-sm dark:text-white mb-2">Shortage/Overage of Quantity:</label>
+        <input type="text" id="shortage_overage_qty" v-model="formData.shortage_overage_qty" class="shadow border  dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="Supplier" class="block text-gray-700 text-sm dark:text-white mb-2">Supplier:</label>
-        <input type="text" id="Supplier" v-model="formData.supplier" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+        <label for="shortage_overage_value" class="block text-gray-700 text-sm dark:text-white mb-2">Shortage/Overage of Value:</label>
+        <input type="text" id="shortage_overage_value" v-model="formData.shortage_overage_value" class="shadow border  dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="Date_Request" class="block text-gray-700 text-sm dark:text-white mb-2">Date Request for Fund:</label>
-        <input type="date" id="Date_Request" v-model="formData.date_request_for_fund" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+        <label for="status" class="block text-gray-700 text-sm dark:text-white mb-2">Status:</label>
+        <input type="text" id="status" v-model="formData.status" class="shadow border  dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
       <div>
-        <label for="Ideal_No" class="block text-gray-700 text-sm dark:text-white mb-2">Ideal No. of Days to Complete:</label>
-        <input type="text" id="Ideal_No" :value="formData.ideal_no_of_days_to_complete" @input="updateIdealDays($event)" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-    </div>
-    <div>
-        <label for="Actual_days" class="block text-gray-700 text-sm dark:text-white mb-2">Actual Days Completed:</label>
-        <input type="text" id="Actual_days" :value="formData.actual_days_to_complete" @input="updateActualDays($event)" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-    </div>
-    <div>
-    <label for="Difference" class="block text-gray-700 text-sm dark:text-white mb-2">Difference:</label>
-    <input type="text" id="Difference" :value="formData.difference" class="shadow border dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline" readonly>
-</div>
-
-      <div>
-        <label for="PurchaseOrder" class="block text-gray-700 text-sm dark:text-white mb-2">Purchase/Work/Job Order:</label>
-        <input type="text" id="PurchaseOrder" v-model="formData.purchase_order" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-      </div>
-      <div>
-        <label for="Delivery_Status" class="block text-gray-700 text-sm dark:text-white mb-2">Delivery Status:</label>
-        <input type="text" id="Delivery_Status" v-model="formData.delivery_status" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
-      </div>
-      <div>
-        <label for="Remarks" class="block text-gray-700 text-sm dark:text-white mb-2">Remarks:</label>
-        <input type="text" id="Remarks" v-model="formData.remarks" class="shadow border  dark:bg-dark-eval-2 rounded w-full py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
+        <label for="remarks_whereabouts" class="block text-gray-700 text-sm dark:text-white mb-2">Remarks & Where Abouts:</label>
+        <input type="text" id="remarks_whereabouts" v-model="formData.remarks_whereabouts" class="shadow border  dark:bg-dark-eval-2 rounded w-5/6 py-2 px-3 text-gray-700  dark:text-white leading-tight focus:outline-none focus:shadow-outline">
       </div>
     
     </div>
