c0d126ef6386f1f31bdf66b509c2a1caa3d86952
ayenmilante18 <144781024+ayenmilante18@users.noreply.github.com>
2023-11-24 20:40:14 +0800

    propertycontroller

---
+++ backend/app/Controllers/PropertyMonitoringController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+use Config\Services;
+
+
+class PropertyMonitoringController extends ResourceController
+{
+    public function index()
+    {
+        //
+    }
+}
