ad3fd4a484976b58fd8e1766bcc5bbf5a2c52479
jkennethdomingo <130147461+jkennethdomingo@users.noreply.github.com>
2023-11-27 15:05:48 +0800

    database update

--- backend/app/Config/Database.php
+++ backend/app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'pdea',
         'password'     => 'capstonepdea',
-        'database'     => 'pdea_capstone',
+        'database'     => 'mainappdev',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
