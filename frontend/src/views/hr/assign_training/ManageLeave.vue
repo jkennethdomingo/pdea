6ef584ea73c6e8f1937156ec2833438b6c782ca9
Jaydee <108048499+JoseDexterAnyayahan@users.noreply.github.com>
2023-12-10 10:55:12 +0800

    Create new modal

--- frontend/src/views/hr/assign_training/ManageLeave.vue
+++ frontend/src/views/hr/assign_training/ManageLeave.vue
@@ -29,12 +29,14 @@ function moveToday() {
   } from '@headlessui/vue'
   
   const isOpen = ref(false)
-  
+  const isOpenDeny = ref(false)
+
   function closeModal() {
     isOpen.value = false
   }
-  function openModal() {
-    isOpen.value = true
+
+  function openModalDeny() {
+    isOpenDeny.value = true
   }
 
 
@@ -505,12 +507,76 @@ function handleWeekendsToggle() {
 </TransitionRoot>
 
 
-          <button
-            @click="denyRequest(post.id)"
-            class="text-white bg-red-600 hover:bg-red-700 rounded-lg text-xs px-4 py-1"
+    <!-- New Deny Button -->
+    <button
+      type="button"
+      @click="openModalDeny"
+      class="text-white bg-red-600 hover:bg-red-700 rounded-lg text-xs px-4 py-1"
+    >
+      Deny
+    </button>
+
+  <TransitionRoot appear :show="isOpenDeny" as="template">
+      <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
           >
-            Deny
-          </button>
+          <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-[#DDE6ED] dark:bg-gray-600 p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-green-800 dark:text-green-200"
+              >
+                Reason for Rejection...
+              </DialogTitle>
+              <div class="mt-2">
+                <textarea 
+                  class="textarea textarea-success w-full border border-green-500 p-2 rounded-md focus:ring focus:ring-green-300 text-gray-800 dark:text-gray-100 bg-white dark:bg-gray-700" 
+                  placeholder="Let us know what's on your mind..."
+                ></textarea>
+              </div>
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-green-600 hover:bg-green-800 px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  @close="closeModal"
+                >
+                  Save
+                </button>
+              </div>
+            </DialogPanel>
+
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+
+
         </div>
       </li>
     </ul>
--- frontend/src/views/hr/beta/Betatest.vue
+++ frontend/src/views/hr/beta/Betatest.vue
@@ -1,48 +1,90 @@
 <template>
-  <VDatePicker v-model="range" is-range>
-    <template #default="{ inputValue, inputEvents }">
-      <div class="flex justify-center items-center">
-        <!-- From Date Input -->
-        <input 
-          type="date"
-          :value="formatDate(inputValue.start)" 
-          @input="inputEvents.start.$event(inputValue.start)"
-          class="shadow border dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="From"
-        />
-        <IconArrowRight />
-        <!-- To Date Input -->
-        <input 
-          type="date"
-          :value="formatDate(inputValue.end)" 
-          @input="inputEvents.end.$event(inputValue.end)"
-          class="shadow border dark:bg-dark-eval-2 rounded w-32 py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="To"
-        />
+  <div class="fixed inset-0 flex items-center justify-center">
+    <button
+      type="button"
+      @click="openModal"
+      class="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+    >
+      Deny
+    </button>
+  </div>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+          <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-[#DDE6ED] dark:bg-gray-600 p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-green-800 dark:text-green-200"
+              >
+                Reason for Rejection...
+              </DialogTitle>
+              <div class="mt-2">
+                <textarea 
+                  class="textarea textarea-success w-full border border-green-500 p-2 rounded-md focus:ring focus:ring-green-300 text-gray-800 dark:text-gray-100 bg-white dark:bg-gray-700" 
+                  placeholder="Let us know what's on your mind..."
+                ></textarea>
+              </div>
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-green-600 hover:bg-green-800 px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  Got it, thanks!
+                </button>
+              </div>
+            </DialogPanel>
+
+          </TransitionChild>
+        </div>
       </div>
-    </template>
-  </VDatePicker>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
 
-const range = ref({
-  start: new Date(2020, 9, 12),
-  end: new Date(2020, 9, 16),
-});
+const isOpen = ref(true)
 
-// Helper function to format Date object to string for input[type=date]
-function formatDate(date) {
-  if (!date) return '';
-  const d = new Date(date);
-  let month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-
-  return [year, month, day].join('-');
+function closeModal() {
+  isOpen.value = false
+}
+function openModal() {
+  isOpen.value = true
 }
 </script>
